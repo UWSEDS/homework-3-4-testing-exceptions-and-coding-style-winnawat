34b6b0f5c3fed862f1b3e2838ef99d3f63b6f714
nawats <win.suvan@gmail.com>
2019-04-30 14:51:29 -0700

    Minor changes in test_dataframe in PEP8 style

--- test_dataframe.py
+++ test_dataframe.py
@@ -8,11 +8,11 @@ class TestDataFrame(unittest.TestCase):
 
     def test_expectedcols(self):
         self.assertTrue(hw2.test_colnames(dfpy.data),
-            msg='DataFrame does not contain expexted columns')
+                        msg='DataFrame does not contain expexted columns')
 
     def test_valuetypes(self):
         self.assertTrue(hw2.test_datatypes(dfpy.data),
-            msg='DataFrame columns are not of expected datatypes')
+                        msg='DataFrame columns are not of expected datatypes')
 
     def test_no_nan(self):
         data = dfpy.data
@@ -27,6 +27,7 @@ class TestDataFrame(unittest.TestCase):
         else:
             rowcheck = False
         self.assertTrue(rowcheck, msg='DataFrame has no row')
+
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestDataFrame)
 _ = unittest.TextTestRunner().run(suite)
